e1e2a83220b294bd56974fa699a10f3effb1848e
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2021-03-23 21:16:41 +0900

    change error message

--- src/js/content.js
+++ src/js/content.js
@@ -2,7 +2,7 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message == "pop_up_alert") {
     window.alert(`
         Cannot download a file.
-        Make sure open a file(which is not a binary-file) on Github.
+        Make sure you are opening a file(which surely has a　filename extension) on Github.
         `);
   }
   if (request.message == "get_raw_url") {
